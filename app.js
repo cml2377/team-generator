26d77b3857a27317c7302e2db948e2d133498c47
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-20 18:38:14 -0600

    Added code here and there; not really organized.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
 const mustache = require('mustache');
 
 const Employee = require("./lib/Employee");
@@ -12,38 +11,74 @@ const Manager = require("./lib/Manager");
 // Welcome to a team information HTML generator!
 //===================================================================
 
+// This array fills in with employee data.
+const teamMembers = [];
+
 //=============================================
 // First, we prompt the user
 //=============================================
 
-async function userInput() {
+async function queryData(userInput) {
     inquirer.prompt()[
-        {
+        {   // Fill html with teamName.
             type: "input",
             message: "What is the name of this team/project?",
             name: "teamName"
         },
-        {
+        {   // There is only 1 manager for a team.
             type: "input",
             message: "Who is the manager of this project?",
             name: "managerName"
         },
-        {
+        {   // Employee ID.
             type: "input",
             message: "What is the manager's ID?",
             name: "managerID"
         },
-        {
+        {   // Employee Title.
             type: "input",
             message: "",
             name: "managerTitle"
         },
+        //=================================================================
+        // This repeats if more employees need to be added.
+        //=================================================================
         {
             type: "rawList",
-            message: "How many Engineers are in the team?",
-            choices: [1, 2, 3, 4]
-        }
-    ]
-}
+            message: "What is this employee's role?",
+            name: "employeeRole",
+            choices: ["Intern", "Engineer"]
+        },
 
-userInput();
+        //==================================================================
+        // These questions are based on the employeeRole above.
+        //==================================================================
+        {
+            type: "input",
+            message: "What is the Engineer's name?",
+            name: "engineerName",
+            when: (userInput) => userInput.employeeRole === "Engineer"
+        },
+        {
+            type: "input",
+            message: "What's the Intern's name?",
+            name: "internName",
+            when: (userInput) => userInput.employeeRole === "Intern"
+        },
+        {
+            type: "confirm",
+            name: "newEmployee",
+            message: "Would you like to add another team member?"
+        },
+    ]
+};
+
+queryData();
+
+app.engine('html', mustache());
+
+function renderEmployees(name, id, title) {
+    var template = document.getElementById('template').innerHTML;
+    var rendered = Mustache.render(template, { name: 'Crystal' });
+    document.getElementById('target').innerHTML = rendered;
+};
